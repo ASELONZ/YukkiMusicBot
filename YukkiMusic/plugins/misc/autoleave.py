eb38d431552ac8eb717634d43f6e7ed7d54d268e
ASELONZ <93417104+ASELONZ@users.noreply.github.com>
2022-03-31 21:56:54 +0530

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -75,7 +75,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
+                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat, Want me back on VC Then Play songs.",
                     )
                 except:
                     continue
